c00a0ca97fb9f1e069dc1e183032274801cb3724
letrongtinh27 <20130440@st.hcmuaf.edu.vn>
2024-06-08 20:40:11 +0700

    update status showtime + fix login google

--- BackEnd/springserver/src/main/java/com/edu/hcmuaf/springserver/config/SecurityConfig.java
+++ BackEnd/springserver/src/main/java/com/edu/hcmuaf/springserver/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -28,79 +29,41 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final AuthenticationProvider authenticationProvider;
 
-
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        req -> req
-
-                                .requestMatchers("/login/oauth2/code/google")
-                                .permitAll()
-                                .requestMatchers("/api/theatres/**")
-                                .permitAll()
-                                .requestMatchers("/api/auth/login_admin/**")
-                                .permitAll()
-                                .requestMatchers("/api/auth/reset-password")
-                                .permitAll()
-                                .requestMatchers("/api/auth/login")
-                                .permitAll()
-                                .requestMatchers("/api/shows/get**")
-                                .permitAll()
-                                .requestMatchers("/api/category/**")
-                                .permitAll()
-                                .requestMatchers("/api/movies/search")
-                                .permitAll()
-                                .requestMatchers("/api/movies/{id}")
-                                .permitAll()
-                                .requestMatchers("/api/theatres/**")
-                                .permitAll()
-                                .requestMatchers("/api/shows/get**")
-                                .permitAll()
-//
-//                                .requestMatchers("/api/shows/all")
-//                                .permitAll()
-//                                .requestMatchers("/api/shows/**")
-//                                .permitAll()
-//                                .requestMatchers("/api/users/**")
-//                                .permitAll()
-//                                .requestMatchers("/api/movie_category/**")
-//                                .permitAll()
-//                                .requestMatchers("/api/tickets/**")
-//                                .permitAll()
-                                .requestMatchers("/api/auth/login/**", "/api/auth/register/**", "/api/auth/login-google")
-                                .permitAll()
-                                .requestMatchers("/api/**").hasAnyAuthority("admin")
-
-                                .requestMatchers("/api/users/**").hasAnyAuthority("user")
-                                .requestMatchers("/api/shows/**").hasAnyAuthority("user")
-                                .requestMatchers("/api/auth/**").hasAnyAuthority("user")
-                                .requestMatchers("/api/seats/**").hasAnyAuthority("user")
-                                .requestMatchers("/api/payment/**").hasAnyAuthority("user")
-                                .requestMatchers("/api/tickets/**").hasAnyAuthority("user")
-
-//                                .requestMatchers("/api/**").hasAnyAuthority("user")
-
-                                .anyRequest()
-                                .authenticated()
-
-
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers("/api/theatres/**").permitAll()
+                        .requestMatchers("/api/auth/reset-password").permitAll()
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/shows/get**").permitAll()
+                        .requestMatchers("/api/category/**").permitAll()
+                        .requestMatchers("/api/movies/search").permitAll()
+                        .requestMatchers("/api/movies/{id}").permitAll()
+                        .requestMatchers("/api/auth/login/**",
+                                "/api/auth/register/**",
+                                "/api/auth/login-google",
+                                "/api/users/profile",
+                                "/api/auth/login_admin/",
+                                "/api/auth/reset-password"
+                        ).permitAll()
+                        .requestMatchers("/api/**").hasAnyAuthority("admin")
+                        .requestMatchers("/api/users/**").hasAnyAuthority("user")
+                        .requestMatchers("/api/shows/**").hasAnyAuthority("user")
+                        .requestMatchers("/api/auth/**").hasAnyAuthority("user")
+                        .requestMatchers("/api/seats/**").hasAnyAuthority("user")
+                        .requestMatchers("/api/payment/**").hasAnyAuthority("user")
+                        .requestMatchers("/api/tickets/**").hasAnyAuthority("user")
+                        .anyRequest().authenticated()
                 )
-
-
-                .oauth2Login(o -> o
+                .oauth2Login(oauth2 -> oauth2
                         .successHandler(new AuthenticationSuccessHandler())
                 )
-
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(STATELESS))
-
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-
                 .build();
     }
 }
